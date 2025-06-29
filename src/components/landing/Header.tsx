a2377b4bf1816ae14fffb6dbc556d36ad13b13d7
Mujeeb Hussain <132893246+mujeebhashone@users.noreply.github.com>
2025-06-29 18:52:20 +0500

    logo and favicon change

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -137,7 +137,7 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import AnimatedButton from "../ui/AnimatedButton";
-import logo from "../../../public/images/UpdatedLogo.png";
+import logo from "../../../public/images/mainlogo.png";
 import Image from "next/image";
 
 // const Header = () => {
@@ -207,9 +207,10 @@ const Header = () => {
             <Image
               src={logo}
               alt="Dubio Logo"
-              width={80}
-              height={60}
-              className="w-16 h-12 sm:w-20 sm:h-15 md:w-24 md:h-18 lg:w-28 lg:h-21 object-contain"
+              width={100}
+              height={100}
+              className="w-16 h-12 sm:w-20 sm:h-15 md:w-24 md:h-18 lg:w-28 lg:h-20 object-contain"
+              unoptimized
             />
           </div>
 
@@ -219,7 +220,7 @@ const Header = () => {
             <div className="hidden sm:flex items-center gap-3 md:gap-4 lg:gap-6">
               {/* Instagram */}
               <Link
-                href="#"
+                href="https://www.instagram.com/dubio.ai"
                 className="text-white hover:text-gray-300 transition-colors p-2 hover:bg-white/10 rounded-full"
                 aria-label="Follow us on Instagram"
               >
@@ -236,7 +237,7 @@ const Header = () => {
 
               {/* Facebook */}
               <Link
-                href="#"
+                href="https://www.facebook.com/dubioai"
                 className="text-white hover:text-gray-300 transition-colors p-2 hover:bg-white/10 rounded-full"
                 aria-label="Follow us on Facebook"
               >
@@ -253,7 +254,7 @@ const Header = () => {
 
               {/* Twitter */}
               <Link
-                href="#"
+                href="https://twitter.com/dubioai"
                 className="text-white hover:text-gray-300 transition-colors p-2 hover:bg-white/10 rounded-full"
                 aria-label="Follow us on Twitter"
               >
@@ -265,6 +266,23 @@ const Header = () => {
                   viewBox="0 0 24 24"
                 >
                   <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
+                </svg>
+              </Link>
+
+              {/* GitHub */}
+              <Link
+                href="https://github.com/dubio-ai"
+                className="text-white hover:text-gray-300 transition-colors p-2 hover:bg-white/10 rounded-full"
+                aria-label="Follow us on GitHub"
+              >
+                <svg
+                  width="18"
+                  height="18"
+                  className="sm:w-5 sm:h-5"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                 </svg>
               </Link>
             </div>
@@ -365,6 +383,24 @@ const Header = () => {
                   <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
                 </svg>
                 <span className="text-sm font-medium">Twitter</span>
+              </Link>
+
+              {/* GitHub */}
+              <Link
+                href="https://github.com/dubioai"
+                className="flex items-center gap-3 text-white hover:text-gray-300 transition-colors p-3 hover:bg-white/10 rounded-lg"
+                aria-label="Follow us on GitHub"
+                onClick={closeModal}
+              >
+                <svg
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+                <span className="text-sm font-medium">GitHub</span>
               </Link>
             </div>
           </div>
